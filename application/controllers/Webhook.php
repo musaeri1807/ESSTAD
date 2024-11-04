e223dadb1055f59c261ef2935cadede064ac8f2b
musaeri1807 <musaeri1807@gmail.com>
2024-11-04 13:06:38 +0700

    OTP WA

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -23,6 +23,7 @@ class Webhook extends CI_Controller
 		header('Content-Type: application/json; charset=utf-8');
 		$json = file_get_contents('php://input');
 		$data = json_decode($json, true);
+		var_dump($data);
 		$device     = $data['device'];
 		$sender     = $data['sender'];
 		$message    = $data['message'];
