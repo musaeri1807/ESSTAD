2696b23b753fdb5898c62aeecbaab193df4e1149
musaeri1807 <musaeri1807@gmail.com>
2024-11-04 12:40:56 +0700

    OTP WA

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -3,6 +3,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Webhook extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+		$this->load->library('session');
+		$this->load->library('logger');
+		$this->load->library('recaptcha');
+		$this->load->model('Users_model');
+		$this->load->helper('url');
+		// require APPPATH . 'third_party/PHPMailer/Exception.php';
+		// require APPPATH . 'third_party/PHPMailer/PHPMailer.php';
+		// require APPPATH . 'third_party/PHPMailer/SMTP.php';
+	}
 
 
 	public function index()
