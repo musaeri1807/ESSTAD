984c623d568e967e7974404760ff446c82345b03
musaeri1807 <musaeri1807@gmail.com>
2025-08-14 16:28:52 +0700

    API

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -12,109 +12,16 @@ class Webhook extends CI_Controller
 		$this->load->library('recaptcha');
 		$this->load->model('Users_model');
 		$this->load->helper('url');
-		// require APPPATH . 'third_party/PHPMailer/Exception.php';
-		// require APPPATH . 'third_party/PHPMailer/PHPMailer.php';
-		// require APPPATH . 'third_party/PHPMailer/SMTP.php';
+
 	}
 
 
 	public function index()
 	{
-		$status = json_decode(file_get_contents('php://input'));
-
-		// header('Content-Type: application/json; charset=utf-8');
-		// $json = file_get_contents('php://input');
-		// $data = json_decode($json, true);
-		// $status = 'pending';
-		$this->db->insert('tbluserlog', ['field_aktifitas' => $status]);
-		die();
-		var_dump($data);
-		$device     = $data['device'];
-		$sender     = $data['sender'];
-		$message    = $data['message'];
-		$text       = $data['text']; //button text
-		$member     = $data['member']; //group member who send the message
-		$name       = $data['name'];
-		$location   = $data['location'];
-		$pollname   = $data['pollname'];
-		$choices    = $data['choices'];
-
-		//data below will only received by device with all feature package
-		//start
-		$url =  $data['url'];
-		$filename =  $data['filename'];
-		$extension =  $data['extension'];
-		//end
-
-		function sendFonnte($target, $data)
-		{
-			$curl = curl_init();
-
-			curl_setopt_array($curl, array(
-				CURLOPT_URL => "https://api.fonnte.com/send",
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_ENCODING => "",
-				CURLOPT_MAXREDIRS => 10,
-				CURLOPT_TIMEOUT => 0,
-				CURLOPT_FOLLOWLOCATION => true,
-				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-				CURLOPT_CUSTOMREQUEST => "POST",
-				CURLOPT_POSTFIELDS => array(
-					'target'    => $target,
-					'message'   => $data['message'],
-					'url'       => $data['url'],
-					'filename'  => $data['filename'],
-				),
-				CURLOPT_HTTPHEADER => array(
-					"Authorization: 7Xb5CgpCxzxBcwgsuRkE"
-				),
-			));
-
-			$response = curl_exec($curl);
-
-			curl_close($curl);
-
-			return $response;
-		}
-
-		if ($message == "test") {
-			$reply = [
-				"message" => "working great!",
-			];
-		} elseif ($message == "image") {
-			$reply = [
-				"message" => "image message",
-				"url" => "https://filesamples.com/samples/image/jpg/sample_640%C3%97426.jpg",
-			];
-		} elseif ($message == "audio") {
-			$reply = [
-				"message" => "audio message",
-				"url" => "https://filesamples.com/samples/audio/mp3/sample3.mp3",
-				"filename" => "music",
-			];
-		} elseif ($message == "video") {
-			$reply = [
-				"message" => "video message",
-				"url" => "https://filesamples.com/samples/video/mp4/sample_640x360.mp4",
-			];
-		} elseif ($message == "file") {
-			$reply = [
-				"message" => "file message",
-				"url" => "https://filesamples.com/samples/document/docx/sample3.docx",
-				"filename" => "document",
-			];
-		} else {
-			$reply = [
-				"message" => "Sorry, i don't understand. Please use one of the following keyword :
-		    
-				Test
-				Audio
-				Video
-				Image
-				File",
-			];
-		}
-
-		sendFonnte($sender, $reply);
+		$notif = json_decode(file_get_contents('php://input'));
+		$this->db->insert('tbluserlog', [
+			'field_waktu' => date('Y-m-d H:i:s'),
+			'field_aktifitas' => json_encode($notif) // simpan jadi JSON string
+		]);
 	}
 }
